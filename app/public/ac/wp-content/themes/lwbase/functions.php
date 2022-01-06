6ee49bdaa6de141c679e5e31da9ef816b7471bd3
roughlang <roughlangx@gmail.com>
2022-01-06 10:25:29 +0900

    add icatch on rest-api(2)

--- app/public/ac/wp-content/themes/lwbase/functions.php
+++ app/public/ac/wp-content/themes/lwbase/functions.php
@@ -5,8 +5,13 @@ function twpp_setup_theme() {
 add_action( 'after_setup_theme', 'twpp_setup_theme' );
 
 /**
- * add icatch url
+ * Wordpress rest api
+ * 
+ * + add icatch url
+ * - delete revisions
  */
+
+ /* add icatch url */
 add_action( 'rest_api_init', 'api_add_fields' );
 function api_add_fields() {
   register_rest_field( 'post',
