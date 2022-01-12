e607b11360327b24e430dc858e13b860cdb41858
roughlang <roughlangx@gmail.com>
2022-01-12 19:35:43 +0900

    相対パスに変更

--- app/public/ac/wp-content/themes/lwbase/include/nav.php
+++ app/public/ac/wp-content/themes/lwbase/include/nav.php
@@ -7,7 +7,7 @@
 		<div class="collapse navbar-collapse" id="navbarScroll">
       <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="<?php echo WP_TOPURL; ?>">Home</a>
+          <a class="nav-link active" aria-current="page" href="/">Home</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle active" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Blog</a>
